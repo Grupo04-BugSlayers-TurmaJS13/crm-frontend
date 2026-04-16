65bd95162ec65f364a1d3f2c3ee6fab9094bcc50
clarisse <clarissebleasby@gmail.com>
2026-04-16 14:37:54 -0300

    ✨ New Feature: Criação dos componentes NavBar e Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+
 
 function App() {
  
   return (
     <>
+    <BrowserRouter>
+
+      <Navbar />
       
+      <Footer />
+
+    </BrowserRouter>
     </>
   )
 }
---
+++ src/Components/navbar/Navbar.tsx
@@ -0,0 +1,43 @@
+import {  UserIcon } from "@phosphor-icons/react"
+import { Link } from "react-router-dom"
+import imgLogoLine from "../../assets/img/conecta-inline.png"
+
+
+function Navbar() {
+
+  
+    return (
+        <>
+            <header className="fixed top-0 left-0 right-0 z-50 bg-(--color-primary-dark) shadow-md text-white border-b ">
+                <div className="w-full px-6 h-16 flex items-center justify-between">
+
+                    <div className="container flex justify-between mx-6">
+
+                        <Link to="/" >
+                            <img 
+                            src={imgLogoLine}
+                            alt="Logo Conecta CRM" 
+                            className="h-7"  
+                            />
+                        </Link>
+
+                        <div className='flex gap-4 text-sm font-light items-center'>
+                            <Link to='/clientes' className=' hover:text-(--color-blue-light) '>CLIENTES</Link>
+                            <Link to='/oportunidades' className='hover:text-(--color-blue-light)'>OPORTUNIDADES</Link>
+                            <button
+                                aria-label="Perfil do usuário"
+                                className="text-white hover:text-(--color-blue-light) transition-colors ml-2"
+                            >
+                                <UserIcon size={26} />
+                            </button>
+
+                        </div>
+                    </div>
+                </div>
+            </header>
+
+        </>
+    )
+}
+
+export default Navbar
