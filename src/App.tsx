f1d9b1f8021c082cd45e9416f268d24958366d7d
SabrinaNovaes <sabrinanovaes96@gmail.com>
2026-04-17 01:24:01 -0300

    ✨ New Feature:  Implementação das Páginas Login e Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/login/Login";
+import { AuthProvider } from "./context/AuthContext";
+import Cadastro from "./pages/cadastro/Cadastro";
 
 function App() {
- 
   return (
     <>
-      
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/cadastrar" element={<Cadastro />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/models/UsuarioLogin.ts
+++ src/models/UsuarioLogin.ts
@@ -1,8 +1,8 @@
-export default interface Usuario {
+export default interface UsuarioLogin {
     id: number
     nome: string
     usuario: string
     senha: string
     foto: string
-    // oportunidades?: Oportunidades[] | null
+    token: string
 }
